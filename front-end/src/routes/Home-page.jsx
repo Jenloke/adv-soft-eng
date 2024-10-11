f77807e40b231f9815514d8f2f782d0528b3f080
Jenloke Magbojos <mjenloke@gmail.com>
2024-10-11 23:26:12 +0800

    supabase query NO RLS

--- front-end/src/routes/Home-page.jsx
+++ front-end/src/routes/Home-page.jsx
@@ -1,8 +1,24 @@
 import Sidebar from "../components/Sidebar.jsx";
 
+import supabase from '../utils/supabase.js'
+import { useState, useEffect } from 'react'
+
 export default function Home() {
+  const [lab, setLab] = useState([])
+
+  useEffect(() => {
+    const getLab = async () => {
+      const { data: Laboratory } = await supabase.from('Laboratory').select('*')
+      return Laboratory
+    }
+
+    setLab(getLab())
+  }, [])
+
+
   return(
     <>
+      <button onClick={() => console.log(lab)}>123</button>
       <div style={{ display: 'flex', minHeight: '100vh' }}>
         <Sidebar/>
         <div style={{ flexGrow: 1, padding: '1rem' }}>
