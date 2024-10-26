730f88a86c88c18b01f4360a9df4609e06d6c45d
Jenloke Magbojos <mjenloke@gmail.com>
2024-10-26 12:04:50 +0800

    basic new equipment insert to supabase

--- src/routes/Form-page.jsx
+++ src/routes/Form-page.jsx
@@ -2,72 +2,93 @@
 import Sidebar from "../components/Sidebar.jsx"
 
 import supabase from '../utils/supabase.js'
-import { useForm, Controller } from "react-hook-form"
 
-import TextField from '@mui/material/TextField';
+import { Controller, useForm } from "react-hook-form"
+
 import Autocomplete from '@mui/material/Autocomplete';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+// Date Pickers
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 export default function Form() {
   const {
     control,
-    register,
     handleSubmit,
+    register,
     // watch,
     formState: { errors },
   } = useForm()
 
-  const onSubmit = (form_inputs) => {
-    console.log(form_inputs)
-    // const { data, error } = supabase.auth.signUp(form_inputs)
-    // if (!error) console.log(data, error)
-  }
-    
-  const signOut = () => {
-    // const { error } = supabase.auth.signOut()
-    // console.log(error)
-  }
-  
   // console.log(watch("email")) // watch input value by passing the name of it
 
+  const onSubmit = async (formInputs) => {
+    const submitInputs = {
+      PropertyNumber: formInputs.propertyNumber,
+      Category: formInputs.category,
+      DateAquisition: formInputs.dateOfAquisition,
+      Location: formInputs.room,
+      Campus: formInputs.campus,
+      LabTech: formInputs.officer,
+    }
+    const { data, error } = await supabase
+      .from('Equipment')
+      .insert([
+        { ...submitInputs, Status: "Functional" },
+      ])
+    await console.log(data)
+    await console.log(error)    
+  }
+  
   return(
     <>
       <div className='flex min-h-screen'>
         <Sidebar/>
-        <div style={{ flexGrow: 1, padding: '1rem' }}>
-          <h1>Forms Page</h1>
-          <form className='' onSubmit={handleSubmit(onSubmit)}>
-            {/* register your input into the hook by invoking the "register" function */}
-            <input defaultValue="test" {...register("email")} />
-
-            {/* include validation with required or other standard HTML validation rules */}
-            <input {...register("password", { required: true })} />
-            {/* errors will return when field validation fails  */}
-            {errors.exampleRequired && <span>This field is required</span>}
+        <div className="m-auto w-1/5">
+          <Button onClick={ async () => {
+            const { data } = await supabase.auth.signInWithPassword({email: 'test@example.com', password: '12345'})
+            await console.log(data)
+          }} 
+          variant="contained">
+            LOGIN
+          </Button>
+          <h1>Add Equipment</h1>
+          <form className='flex-col space-y-4' onSubmit={handleSubmit(onSubmit)}>
+            {/* <input defaultValue="test" {...register("email")} /> */}
+            {/* <input {...register("password", { required: true })} /> */}
+            {/* {errors.exampleRequired && <span>This field is required</span>} */}
             
-            <Controller
-              control={control}
-              rules={{
-                maxLength: 100,
-              }}
-              render={({ field: { onChange, onBlur, value } }) => (
-                <TextField
-                  placeholder="Last name"
-                  onBlur={onBlur}
-                  onChangeText={onChange}
-                  value={value}
-                />
-              )}
-              name="lastName"
-            />
-
             <TextField
-              
-              id="outlined-error-helper-text"
-              label="Error"
-              defaultValue="Hello World"
-              helperText="Incorrect entry."
+              className='w-full'
+              {...register("propertyNumber", { required: true })}
+              label="Property Number"
+              error={errors.exampleRequired}
+              helperText={errors.exampleRequired && "Input Required"}
             />
-
+            <TextField
+              className='w-full'
+              {...register("category", { required: true })}
+              label="Category"
+              error={errors.exampleRequired}
+              helperText={errors.exampleRequired && "Input Required"}
+            />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <Controller
+                name="dateOfAquisition"
+                control={control}
+                render={({ field }) => (
+                  <DatePicker
+                    className='w-full'
+                    label="Select Date"
+                    value={field.value}
+                    onChange={(newValue) => field.onChange(newValue)} // Pass new value to field's onChange
+                  />
+                )}
+              />
+            </LocalizationProvider>
             <Autocomplete
               freeSolo
               options={['Cisco Laboratory', 'Multimedia Laboratory', 'Software Laboratory 1', 'Software Laboratory 2', 'Software Laboratory 3', 'Info-tech Laboratory']}
@@ -78,11 +99,31 @@ export default function Form() {
                   />
               }
             />
-
-            <input type="submit" />
+            <Autocomplete
+              freeSolo
+              options={['Alangilan', 'Pablo Borbon']}
+              renderInput={
+                (params) => 
+                  <TextField 
+                    {...register("campus", { required: true })} {...params} label="Campus" 
+                  />
+              }
+            />
+            <Autocomplete
+              freeSolo
+              options={['Jaycee C. Aurelio', 'John Pol M. Jalapan', 'Alain Micko C. Moreno']}
+              renderInput={
+                (params) => 
+                  <TextField 
+                    {...register("officer", { required: true })} {...params} label="ICT Officer In Charge" 
+                  />
+              }
+            />
+            <Button type="submit" variant="contained">
+              SUBMIT
+            </Button>
           </form>
           
-          <button onClick={signOut}>Sign Out</button>
 
         </div>
       </div>
