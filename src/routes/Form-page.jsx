21af5a11be456d7f350394285a299b868da205f4
Jenloke Magbojos <mjenloke@gmail.com>
2024-10-27 01:02:24 +0800

    update more inputs but no validation yet

--- src/routes/Form-page.jsx
+++ src/routes/Form-page.jsx
@@ -26,6 +26,7 @@ export default function Form() {
   // console.log(watch("email")) // watch input value by passing the name of it
 
   const onSubmit = async (formInputs) => {
+    console.log(formInputs)
     const submitInputs = {
       PropertyNumber: formInputs.propertyNumber,
       Category: formInputs.category,
@@ -34,95 +35,224 @@ export default function Form() {
       Campus: formInputs.campus,
       LabTech: formInputs.officer,
     }
-    const { data, error } = await supabase
-      .from('Equipment')
-      .insert([
-        { ...submitInputs, Status: "Functional" },
-      ])
-    await console.log(data)
-    await console.log(error)    
+    // const { data, error } = await supabase
+    //   .from('Equipment')
+    //   .insert([
+    //     { ...submitInputs, Status: "Functional" },
+    //   ])
+    // console.log(data)
+    // console.log(error)    
   }
   
   return(
     <>
       <div className='flex min-h-screen'>
         <Sidebar/>
-        <div className="m-auto w-1/5">
-          <Button onClick={ async () => {
-            const { data } = await supabase.auth.signInWithPassword({email: 'test@example.com', password: '12345'})
-            await console.log(data)
-          }} 
-          variant="contained">
+        <div className="mx-auto">
+          <Button 
+            onClick={ async () => {
+              const { data } = await supabase.auth.signInWithPassword({email: 'test@example.com', password: '12345'})
+              console.log(data)
+            }} 
+            variant="contained"
+          >
             LOGIN
           </Button>
+
           <h1>Add Equipment</h1>
-          <form className='flex-col space-y-4' onSubmit={handleSubmit(onSubmit)}>
+
+          <form className='flex-col space-y-10' onSubmit={handleSubmit(onSubmit)}>
             {/* <input defaultValue="test" {...register("email")} /> */}
             {/* <input {...register("password", { required: true })} /> */}
             {/* {errors.exampleRequired && <span>This field is required</span>} */}
             
-            <TextField
-              className='w-full'
-              {...register("propertyNumber", { required: true })}
-              label="Property Number"
-              error={errors.exampleRequired}
-              helperText={errors.exampleRequired && "Input Required"}
-            />
-            <TextField
-              className='w-full'
-              {...register("category", { required: true })}
-              label="Category"
-              error={errors.exampleRequired}
-              helperText={errors.exampleRequired && "Input Required"}
-            />
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <Controller
-                name="dateOfAquisition"
-                control={control}
-                render={({ field }) => (
-                  <DatePicker
-                    className='w-full'
-                    label="Select Date"
-                    value={field.value}
-                    onChange={(newValue) => field.onChange(newValue)} // Pass new value to field's onChange
-                  />
-                )}
+            <div className='space-y-3'>
+              <h4 className='text-center'>
+                Information
+              </h4>
+              <TextField
+                className='w-full'
+                {...register("propertyNumber", { required: true })}
+                label="Property Number"
+                error={errors.exampleRequired}
+                helperText={errors.exampleRequired && "Input Required"}
               />
-            </LocalizationProvider>
-            <Autocomplete
-              freeSolo
-              options={['Cisco Laboratory', 'Multimedia Laboratory', 'Software Laboratory 1', 'Software Laboratory 2', 'Software Laboratory 3', 'Info-tech Laboratory']}
-              renderInput={
-                (params) => 
-                  <TextField 
-                    {...register("room", { required: true })} {...params} label="Laboratory" 
-                  />
-              }
-            />
-            <Autocomplete
-              freeSolo
-              options={['Alangilan', 'Pablo Borbon']}
-              renderInput={
-                (params) => 
-                  <TextField 
-                    {...register("campus", { required: true })} {...params} label="Campus" 
-                  />
-              }
-            />
-            <Autocomplete
-              freeSolo
-              options={['Jaycee C. Aurelio', 'John Pol M. Jalapan', 'Alain Micko C. Moreno']}
-              renderInput={
-                (params) => 
-                  <TextField 
-                    {...register("officer", { required: true })} {...params} label="ICT Officer In Charge" 
-                  />
-              }
-            />
+              <TextField
+                className='w-full'
+                {...register("category", { required: true })}
+                label="Category"
+                error={errors.exampleRequired}
+                helperText={errors.exampleRequired && "Input Required"}
+              />
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <Controller
+                  name="dateOfAquisition"
+                  control={control}
+                  render={({ field }) => (
+                    <DatePicker
+                      className='w-full'
+                      label="Select Date"
+                      value={field.value}
+                      onChange={(newValue) => field.onChange(newValue)} 
+                    />
+                  )}
+                />
+              </LocalizationProvider>
+              <Autocomplete
+                freeSolo
+                options={['Cisco Laboratory', 'Multimedia Laboratory', 'Software Laboratory 1', 'Software Laboratory 2', 'Software Laboratory 3', 'Info-tech Laboratory']}
+                renderInput={
+                  (params) => 
+                    <TextField
+                      className='w-full'
+                      {...register("room", { required: true })} {...params} label="Laboratory" 
+                    />
+                }
+              />
+              <Autocomplete
+                freeSolo
+                options={['Alangilan', 'Pablo Borbon']}
+                renderInput={
+                  (params) => 
+                    <TextField 
+                      className='w-full'
+                      {...register("campus", { required: true })} {...params} label="Campus" 
+                    />
+                }
+              />
+              <Autocomplete
+                freeSolo
+                options={['Jaycee C. Aurelio', 'Lohren Joshua L. Geron', 'John Pol M. Jalapan', 'Alain Micko C. Moreno']}
+                renderInput={
+                  (params) => 
+                    <TextField 
+                      className='w-full'
+                      {...register("officer", { required: true })} {...params} label="ICT Officer In Charge" 
+                    />
+                }
+              />
+            </div>
+
+            <div className='space-y-3'>
+              <h4 className='text-center'>
+                Device Specification
+              </h4>
+              <Autocomplete
+                freeSolo
+                options={['INTEL CORE I7-12700 2.10 GHz (12Cores)', 'INTEL CORE I5-7400 3.00 GHz (4Cores)']}
+                renderInput={
+                  (params) => 
+                    <TextField 
+                      className='w-1/5'
+                      {...register("processor", { required: true })} {...params} label="Processor" 
+                    />
+                }
+              />
+              <Autocomplete
+                freeSolo
+                options={['XITRIX XPN-H610M2', 'ASUS PRIME B250M-A']}
+                renderInput={
+                  (params) => 
+                    <TextField 
+                      className='w-1/5'
+                      {...register("motherboard", { required: true })} {...params} label="Motherboard" 
+                    />
+                }
+              />
+              <Autocomplete
+                freeSolo
+                options={['1TB + 250GB SSD', '1TB']}
+                renderInput={
+                  (params) => 
+                    <TextField 
+                      className='w-1/5'
+                      {...register("hdd", { required: true })} {...params} label="HDD" 
+                    />
+                }
+              />
+              <Autocomplete
+                freeSolo
+                options={['16GB DDR4', 'KINGSTON 4G DDR4']}
+                renderInput={
+                  (params) => 
+                    <TextField 
+                      className='w-1/5'
+                      {...register("memory", { required: true })} {...params} label="Memory" 
+                    />
+                }
+              />
+              <Autocomplete
+                freeSolo
+                options={['NVIDIA GeForce GTX 1660 SUPER', 'INTEL HD GRAPHICS 630']}
+                renderInput={
+                  (params) => 
+                    <TextField 
+                      className='w-1/5'
+                      {...register("videoCard", { required: true })} {...params} label="Video Card" 
+                    />
+                }
+              />
+              <Autocomplete
+                freeSolo
+                options={['G24 24" 165Hz Curved XITRIX Monitor', 'AOC 19.53" LED MONITOR']}
+                renderInput={
+                  (params) => 
+                    <TextField 
+                      className='w-1/5'
+                      {...register("display", { required: true })} {...params} label="Display" 
+                    />
+                }
+              />
+              <Autocomplete
+                freeSolo
+                options={['HAVE', 'NONE']}
+                renderInput={
+                  (params) => 
+                    <TextField 
+                      className='w-1/5'
+                      {...register("opticalDrive", { required: true })} {...params} label="Optical Drive" 
+                    />
+                }
+              />
+              <Autocomplete
+                freeSolo
+                options={['XITRIX Mid Tower RGB Chassis w/ Tempered', 'COOLERMASTER']}
+                renderInput={
+                  (params) => 
+                    <TextField 
+                      className='w-1/5'
+                      {...register("casing", { required: true })} {...params} label="Casing" 
+                    />
+                }
+              />
+              <Autocomplete
+                freeSolo
+                options={['Xitrix Ergonomic Design USB Mouse', 'A4TECH USB']}
+                renderInput={
+                  (params) => 
+                    <TextField 
+                      className='w-1/5'
+                      {...register("mouse", { required: true })} {...params} label="Mouse" 
+                    />
+                }
+              />
+              <Autocomplete
+                freeSolo
+                options={['Xitrix Ergonomic Design USB Keyboard', 'A4TECH USB']}
+                renderInput={
+                  (params) => 
+                    <TextField 
+                      className='w-1/5'
+                      {...register("keyboard", { required: true })} {...params} label="Keyboard" 
+                    />
+                }
+              />
+            </div>
             <Button type="submit" variant="contained">
-              SUBMIT
+              ADD Equipment
             </Button>
           </form>
+
           
 
         </div>
