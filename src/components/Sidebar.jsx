c50035ba6e6d7fb0cbbe29771ba9333343b27da5
Jenloke Magbojos <mjenloke@gmail.com>
2024-11-04 21:46:19 +0800

    implement prettier

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,47 +1,63 @@
-import { CDBSidebar, CDBSidebarContent, CDBSidebarFooter, CDBSidebarHeader, CDBSidebarMenu, CDBSidebarMenuItem} from 'cdbreact';
-import { NavLink } from "react-router-dom";
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact'
+import { NavLink } from 'react-router-dom'
 
-export default function Sidebar(){
-    return(
-        <>
-            <div style={{ display: 'flex', minHeight: '100vh', overflow: 'scroll initial'}}>
-                <CDBSidebar textColor="#d1d1d1" backgroundColor="#ff2e2e">
-                    <CDBSidebarHeader prefix={<i className="fa fa-bars fa-small"></i>}>
-                        <span className="text-decoration-none" style={{ color: 'inherit', fontSize: '0.85rem' }}>
-                            Batangas State University
-                        </span>
-                    </CDBSidebarHeader>
+export default function Sidebar() {
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          minHeight: '100vh',
+          overflow: 'scroll initial',
+        }}
+      >
+        <CDBSidebar textColor="#d1d1d1" backgroundColor="#ff2e2e">
+          <CDBSidebarHeader prefix={<i className="fa fa-bars fa-small"></i>}>
+            <span
+              className="text-decoration-none"
+              style={{ color: 'inherit', fontSize: '0.85rem' }}
+            >
+              Batangas State University
+            </span>
+          </CDBSidebarHeader>
 
-                    <CDBSidebarContent className="sidebar-content">
-                        <CDBSidebarMenu>
-                            <NavLink exact to="/" activeClassName="activeClicked">
-                                <CDBSidebarMenuItem icon="columns">Home</CDBSidebarMenuItem>
-                            </NavLink>
-                            <NavLink exact to="/table" activeClassName="activeClicked">
-                                <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
-                            </NavLink>
-                            <NavLink exact to="/form" activeClassName="activeClicked">
-                                <CDBSidebarMenuItem icon="user">Forms</CDBSidebarMenuItem>
-                            </NavLink>
-                            {/* <NavLink exact to="/analytics" activeClassName="activeClicked">
+          <CDBSidebarContent className="sidebar-content">
+            <CDBSidebarMenu>
+              <NavLink exact to="/" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="columns">Home</CDBSidebarMenuItem>
+              </NavLink>
+              <NavLink exact to="/table" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
+              </NavLink>
+              <NavLink exact to="/form" activeClassName="activeClicked">
+                <CDBSidebarMenuItem icon="user">Forms</CDBSidebarMenuItem>
+              </NavLink>
+              {/* <NavLink exact to="/analytics" activeClassName="activeClicked">
                                 <CDBSidebarMenuItem icon="chart-line">Analytics</CDBSidebarMenuItem>
                             </NavLink> */}
-                        </CDBSidebarMenu>
-                    </CDBSidebarContent>
+            </CDBSidebarMenu>
+          </CDBSidebarContent>
 
-                    <CDBSidebarFooter style={{ textAlign: 'center' }}>
-                    <div
-                        style={{
-                        padding: '20px 5px',
-                        }}
-                    >
-                        <NavLink exact to="/login" activeClassName="activeClicked">
-                            Account
-                        </NavLink>
-                    </div>
-                    </CDBSidebarFooter>
-                </CDBSidebar>
+          <CDBSidebarFooter style={{ textAlign: 'center' }}>
+            <div
+              style={{
+                padding: '20px 5px',
+              }}
+            >
+              <NavLink exact to="/login" activeClassName="activeClicked">
+                Account
+              </NavLink>
             </div>
-        </>
-    )
+          </CDBSidebarFooter>
+        </CDBSidebar>
+      </div>
+    </>
+  )
 }
