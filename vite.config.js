2e48edbd9e2e77bda0f32afb36975376fe0bb9f4
Jenloke Magbojos <mjenloke@gmail.com>
2024-11-23 11:31:51 +0800

    remove comments

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) =>{
   const config = {
     plugins: [react()],
-    // server: {
-    //   host: true
-    // },
+    server: {
+      host: true
+    },
     base: "/", 
   }
 
