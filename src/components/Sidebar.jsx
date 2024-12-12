f17f916838f71e3b11a67a2deab63b7ad9a9f0cf
Jenloke Magbojos <mjenloke@gmail.com>
2024-12-13 01:01:42 +0800

    change color and icons, move logout to here

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,41 +7,54 @@ import {
 } from 'cdbreact'
 import { NavLink } from 'react-router-dom'
 
-export default function Sidebar() {
-  return (
-    <>
-      <div
-        style={{
-          display: 'flex',
-          minHeight: '100vh',
-          overflow: 'scroll initial',
-        }}
-      >
-        <CDBSidebar textColor="#d1d1d1" backgroundColor="#c71a1a">
-          <CDBSidebarHeader prefix={<i className="fa fa-bars fa-small"></i>}>
-            <span
-              className="text-decoration-none"
-              style={{ color: 'inherit', fontSize: '0.85rem' }}
-            >
-              Batangas State University
-            </span>
-          </CDBSidebarHeader>
+import supabase from '../utils/supabase.js'
+import { useNavigate } from 'react-router-dom'
 
-          <CDBSidebarContent className="sidebar-content">
-            <CDBSidebarMenu>
-              <NavLink exact to="/" className="">
-                <CDBSidebarMenuItem icon="columns">Home</CDBSidebarMenuItem>
-              </NavLink>
-              <NavLink exact to="/labs" className="">
-                <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
-              </NavLink>
-              <NavLink exact to="/form" className="">
-                <CDBSidebarMenuItem icon="user">Forms</CDBSidebarMenuItem>
-              </NavLink>
-            </CDBSidebarMenu>
-          </CDBSidebarContent>
-        </CDBSidebar>
-      </div>
-    </>
+export default function Sidebar() {
+  const navigate = useNavigate()
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        minHeight: '100vh',
+        overflow: 'scroll initial',
+      }}
+    >
+      <CDBSidebar textColor="#ffffff" backgroundColor="#1976d2">
+        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-small"></i>}>
+          <span
+            className="text-decoration-none"
+            style={{ color: 'inherit', fontSize: '0.85rem' }}
+          >
+            Batangas State University
+          </span>
+        </CDBSidebarHeader>
+
+        <CDBSidebarContent className="sidebar-content">
+          <CDBSidebarMenu>
+            <NavLink exact to="/" className="">
+              <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/labs" className="">
+              <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/form" className="">
+              <CDBSidebarMenuItem icon="file">Forms</CDBSidebarMenuItem>
+            </NavLink>
+            <CDBSidebarMenuItem
+              onClick={async () => {
+                const { error } = await supabase.auth.signOut()
+                if (error) console.log(error)
+                navigate('/login')
+              }}
+              icon="lock"
+            >
+              Logout
+            </CDBSidebarMenuItem>
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
+      </CDBSidebar>
+    </div>
   )
 }
